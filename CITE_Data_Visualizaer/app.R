b192a7f0679803ecad01d7b7f20afad01a55b577
aomartin923 <aomartin@ucsb.edu>
2022-02-06 14:57:41 -0800

    created inputs for widgets 1 and 2

--- CITE_Data_Visualizaer/app.R
+++ CITE_Data_Visualizaer/app.R
@@ -9,8 +9,9 @@
 
 library(shiny)
 library(tidyverse)
+library(janitor)
 
-wildlife <- read.csv("cites_wildlife_data.csv", stringsAsFactors = FALSE)
+wildlife <- read.csv("cites_wildlife_data.csv", stringsAsFactors = FALSE) 
 
 ## create user interface 
 
@@ -21,14 +22,30 @@ ui <- fluidPage(
              sidebarLayout(
                sidebarPanel(
                  'widget 1 goes here',
-                 checkboxGroupInput(inputId = "wildlife_dist")
-               ),
+                 checkboxGroupInput(inputId = "wildlife_dist",
+                                    label = "Choose Term:",
+                                    choices = c("Importer", "Exporter")
+                                    ) # end checkboxGroupInput
+               ), #end of sidebarPanel
                mainPanel(
                  "output goes here"
                  ) # end of mainPanel
              ), #end of sidebarLayout
             ), #end of tabPanel for widget 1
-    tabPanel("Widget 2"),
+    tabPanel("Widget 2",
+             sidebarLayout(
+               sidebarPanel(
+                 "Widget 2 goes here",
+                 checkboxGroupInput(inputId = "wildlife_dist",
+                                    label = "Choose Term:",
+                                    choices = c("Live","Bodies","Specimens")
+                                    ) #end checkboxGroupOutput
+               ),#end sidebarPanel 
+               mainPanel(
+                 "output goes here"
+               ) #end of mainPanel 
+             ) #end sidebarLayout
+             ), # end of tabPanel
     tabPanel("Widget 3"),
     tabPanel("Widget 4")
   ) # this is end of navbarPage 
